3fdc60ff6b33a6df526703be2d5de32e239ac224
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-11-19 20:44:53 +0100

    User upgrade

--- libs/frontend/features/src/lib/users/user.service.ts
+++ libs/frontend/features/src/lib/users/user.service.ts
@@ -8,25 +8,55 @@ import { IUserInfo, UserGender, UserRole } from "@avans-nx-workshop/shared/api";
 export class UserService {
   readonly users: IUserInfo[] = [
     {
-                _id: '1',
-                name: 'John Doe',
-                emailAddress: 'j.doe@outlook.com',
-                role: UserRole.Unknown,
-                gender: UserGender.Unknown,
-                password: 'secret',
-                isActive: true,
-                profileImgUrl: "url"
-            },
-            {
-                _id: '2',
-                name: 'Jane Doe',
-                emailAddress: 'j.doe2@outlook.com',
-                role: UserRole.Unknown,
-                gender: UserGender.Unknown,
-                password: 'secret',
-                isActive: true,
-                profileImgUrl: "url"
-            }
+        _id: '1',
+      name: 'Sietse Hooft',
+      emailAddress: 's.Hooft@outlook.com',
+      role: UserRole.Unknown,
+      gender: UserGender.Unknown,
+      password: 'SietsHooft',
+      isActive: true,
+      profileImgUrl: "https://picsum.photos/id/1/200/300"
+    },
+    {
+      _id: '2',
+      name: 'Merel van Dijk',
+      emailAddress: 'm.Dijk2@outlook.com',
+      role: UserRole.Guest,
+      gender: UserGender.Unknown,
+      password: 'MerelDijk',
+      isActive: true,
+      profileImgUrl: "https://picsum.photos/id/22/200/300"
+    },
+    {
+      _id: '3',
+      name: 'Jan de Vries',
+      emailAddress: 'j.devries@outlook.com',
+      role: UserRole.Admin,
+      gender: UserGender.Male,
+      password: 'JanVriesland',
+      isActive: true,
+      profileImgUrl: "https://picsum.photos/id/27/200/300"
+    },
+    {
+      _id: '4',
+      name: 'Anna Jansen',
+      emailAddress: 'a.jansen@outlook.com',
+      role: UserRole.Admin,
+      gender: UserGender.Female,
+      password: 'AdamAnna',
+      isActive: false,
+      profileImgUrl: "https://picsum.photos/id/40/200/300"
+    },
+    {
+      _id: '5',
+      name: 'Peter Bakker',
+      emailAddress: 'p.bakker@outlook.com',
+      role: UserRole.Unknown,
+      gender: UserGender.Male,
+      password: 'PeterBakkers',
+      isActive: true,
+      profileImgUrl: "https://picsum.photos/id/64/200/300"
+    }
   ];
 
   constructor() {
@@ -49,7 +79,7 @@ export class UserService {
     console.log('getUsersAsObservable aangeroepen');
     // 'of' is een rxjs operator die een Observable
     // maakt van de gegeven data.
-    return of(this.users).pipe(delay(2000));
+    return of(this.users).pipe(delay(1000));
   }
 
   getUserById(id: string | null): IUserInfo {
@@ -59,6 +89,6 @@ export class UserService {
 
   getuserByIdAsync(id: string | null): Observable<IUserInfo> {
     console.log('getUserByIdAsync aangeroepen');
-    return of(this.users.filter((user) => user._id === id)[0]).pipe(delay(2000));
+    return of(this.users.filter((user) => user._id === id)[0]).pipe(delay(1000));
   }
 }
